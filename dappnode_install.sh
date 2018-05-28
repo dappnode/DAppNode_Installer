a458ff9c725916e192ad662b8c64cb810af7ec21
Unknown <guillem.lion.benosa@gmail.com>
2018-05-28 23:08:31 +0200

    fixed DNCORE dir

--- dappnode_install.sh
+++ dappnode_install.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+###### When incorporating the images from IPFS:
+# echo $URL_LIST | xargs -n 1 -P 8 wget -q
+# ref: https://stackoverflow.com/questions/7577615/parallel-wget-in-bash
 
 # FOR PRODUCTION: Replace links with IPFS
 CORE_URL="https://raw.githubusercontent.com/dappnode/DNCORE/master/docker-compose.yml"
@@ -13,20 +16,19 @@ LIB3_URL="https://raw.githubusercontent.com/dappnode/DN_ISO_Generator/master/bui
 # STEP 0: Declare paths and directories
 # ----------------------------------------
 DAPPNODE_DIR="/usr/src/dappnode/"
-DCKR_DIR="${DAPPNODE_DIR}bin/docker/"
-DCKR_PATH="${DCKR_DIR}docker-ce_17.12.0~ce-0~ubuntu_amd64.deb"
-DCMP_DIR="/usr/local/bin/"
-DCMP_PATH="${DCMP_DIR}docker-compose"
+DCKR_PATH="${DAPPNODE_DIR}bin/docker/docker-ce_17.12.0~ce-0~ubuntu_amd64.deb"
+DCMP_PATH="/usr/local/bin/docker-compose"
 LIB_DIR="${DAPPNODE_DIR}libs/linux/debian/"
 LIB1_PATH="${LIB_DIR}aufs-tools_1%253a3.2%2B20130722-1.1ubuntu1_amd64.deb"
 LIB2_PATH="${LIB_DIR}cgroupfs-mount_1.2_all.deb"
 LIB3_PATH="${LIB_DIR}libltdl7_2.4.6-0.1_amd64.deb"
-CORE_PATH="${DAPPNODE_DIR}docker-compose.yml"
+CORE_PATH="${DAPPNODE_DIR}DNCORE/docker-compose.yml"
 # Ensure paths exist
 mkdir -p $DAPPNODE_DIR
-mkdir -p $DCKR_DIR
-mkdir -p $DCMP_DIR
+mkdir -p $(dirname "$DCKR_PATH")
+mkdir -p $(dirname "$DCMP_PATH")
 mkdir -p $LIB_DIR
+mkdir -p $(dirname "$CORE_PATH")
 
 
 # STEP 1: Download files from a decentralized source
@@ -80,11 +82,10 @@ fi
 
 # STEP 3: Start DAppNode
 # ----------------------------------------
-cd $DAPPNODE_DIR
-docker-compose up -d
+docker-compose -f ${CORE_PATH} up -d
 
 # Testing result
-if docker-compose ps | grep -q "dncore-dnp_ethchain"; then
+if docker-compose -f ${CORE_PATH} ps | grep -q "dncore-dnp_ethchain"; then
     echo -e "${GREEN}\n\nVerified dappnode installation \n\n -------${NC}"
 else
     echo -e "${RED}\n\nERROR:\n  docker-compose ps, does not return the expected packages \n\n -------${NC}"
