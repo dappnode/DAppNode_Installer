7cb49c08daca97ee32a5f075142dff0446e124a8
Eduardo Antuña Díez <eduadiez@gmail.com>
2018-06-02 15:03:46 +0200

    General Improvements

---
+++ build/scripts/download_core.sh
@@ -0,0 +1,59 @@
+#!/bin/bash
+
+source ./versions.sh
+
+DAPPNODE_DIR="/images/"
+
+BIND_URL="https://github.com/dappnode/DNP_BIND/releases/download/v${BIND_VERSION}/bind.dnp.dappnode.eth_${BIND_VERSION}.tar.xz"
+IPFS_URL="https://github.com/dappnode/DNP_IPFS/releases/download/v${IPFS_VERSION}/ipfs.dnp.dappnode.eth_${IPFS_VERSION}.tar.xz"
+ETHCHAIN_URL="https://github.com/dappnode/DNP_ETHCHAIN/releases/download/v${ETHCHAIN_VERSION}/ethchain.dnp.dappnode.eth_${ETHCHAIN_VERSION}.tar.xz"
+ETHFORWARD_URL="https://github.com/dappnode/DNP_ETHFORWARD/releases/download/v${ETHFORWARD_VERSION}/ethforward.dnp.dappnode.eth_${ETHFORWARD_VERSION}.tar.xz"
+VPN_URL="https://github.com/dappnode/DNP_VPN/releases/download/v${VPN_VERSION}/vpn.dnp.dappnode.eth_${VPN_VERSION}.tar.xz"
+WAMP_URL="https://github.com/dappnode/DNP_WAMP/releases/download/v${WAMP_VERSION}/wamp.dnp.dappnode.eth_${WAMP_VERSION}.tar.xz"
+
+BIND_YML="https://github.com/dappnode/DNP_BIND/releases/download/v${BIND_VERSION}/docker-compose-bind.yml"
+IPFS_YML="https://github.com/dappnode/DNP_IPFS/releases/download/v${IPFS_VERSION}/docker-compose-ipfs.yml"
+ETHCHAIN_YML="https://github.com/dappnode/DNP_ETHCHAIN/releases/download/v${ETHCHAIN_VERSION}/docker-compose-ethchain.yml"
+ETHFORWARD_YML="https://github.com/dappnode/DNP_ETHFORWARD/releases/download/v${ETHFORWARD_VERSION}/docker-compose-ethforward.yml"
+VPN_YML="https://github.com/dappnode/DNP_VPN/releases/download/v${VPN_VERSION}/docker-compose-vpn.yml"
+WAMP_YML="https://github.com/dappnode/DNP_WAMP/releases/download/v${WAMP_VERSION}/docker-compose-wamp.yml"
+
+BIND_YML_FILE="${DAPPNODE_DIR}docker-compose-bind.yml"
+IPFS_YML_FILE="${DAPPNODE_DIR}docker-compose-ipfs.yml"
+ETHCHAIN_YML_FILE="${DAPPNODE_DIR}docker-compose-ethchain.yml"
+ETHFORWARD_YML_FILE="${DAPPNODE_DIR}docker-compose-ethforward.yml"
+VPN_YML_FILE="${DAPPNODE_DIR}docker-compose-vpn.yml"
+WAMP_YML_FILE="${DAPPNODE_DIR}docker-compose-wamp.yml"
+
+BIND_FILE="${DAPPNODE_DIR}bind.dnp.dappnode.eth_${BIND_VERSION}.tar.xz"
+IPFS_FILE="${DAPPNODE_DIR}ipfs.dnp.dappnode.eth_${IPFS_VERSION}.tar.xz"
+ETHCHAIN_FILE="${DAPPNODE_DIR}ethchain.dnp.dappnode.eth_${ETHCHAIN_VERSION}.tar.xz"
+ETHFORWARD_FILE="${DAPPNODE_DIR}ethforward.dnp.dappnode.eth_${ETHFORWARD_VERSION}.tar.xz"
+VPN_FILE="${DAPPNODE_DIR}vpn.dnp.dappnode.eth_${VPN_VERSION}.tar.xz"
+WAMP_FILE="${DAPPNODE_DIR}wamp.dnp.dappnode.eth_${WAMP_VERSION}.tar.xz"
+
+dappnode_core_download()
+{
+    [ -f $BIND_FILE ] || wget -O $BIND_FILE $BIND_URL
+    [ -f $IPFS_FILE ] || wget -O $IPFS_FILE $IPFS_URL
+    [ -f $ETHCHAIN_FILE ] || wget -O $ETHCHAIN_FILE $ETHCHAIN_URL
+    [ -f $ETHFORWARD_FILE ] || wget -O $ETHFORWARD_FILE $ETHFORWARD_URL
+    [ -f $VPN_FILE ] || wget -O $VPN_FILE $VPN_URL
+    [ -f $WAMP_FILE ] || wget -O $WAMP_FILE $WAMP_URL
+
+    [ -f $BIND_YML_FILE ] || wget -O $BIND_YML_FILE $BIND_YML
+    [ -f $IPFS_YML_FILE ] || wget -O $IPFS_YML_FILE $IPFS_YML
+    [ -f $ETHCHAIN_YML_FILE ] || wget -O $ETHCHAIN_YML_FILE $ETHCHAIN_YML
+    [ -f $ETHFORWARD_YML_FILE ] || wget -O $ETHFORWARD_YML_FILE $ETHFORWARD_YML
+    [ -f $VPN_YML_FILE ] || wget -O $VPN_YML_FILE $VPN_YML
+    [ -f $WAMP_YML_FILE ] || wget -O $WAMP_YML_FILE $WAMP_YML
+}
+
+echo -e "\e[32mDownloading DAppNode Core...\e[0m"
+dappnode_core_download
+
+echo -e "\e[32mCopying files...\e[0m"
+cp /images/*.tar.xz dappnode/DNCORE
+cp /images/*.yml dappnode/DNCORE
+
+cp ./versions.sh dappnode/scripts
