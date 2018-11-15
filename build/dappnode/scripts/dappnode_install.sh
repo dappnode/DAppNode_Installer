7100c17a6194c406aac132125e2ab064bf595394
Eduardo Antuña Díez <eduadiez@gmail.com>
2018-11-15 10:18:07 +0100

    Added update option

--- build/dappnode/scripts/dappnode_install.sh
+++ build/dappnode/scripts/dappnode_install.sh
@@ -46,6 +46,10 @@ if [[ ! -z $STATIC_IP ]]; then
     fi
 fi
 
+if [ "$UPDATE" = true ] ; then
+    rm -rf ${DAPPNODE_CORE_DIR}
+fi
+
 [ -f $PROFILE_FILE ] || ${WGET} -O ${PROFILE_FILE} ${PROFILE_URL}
 
 source "${PROFILE_FILE}"
@@ -95,7 +99,7 @@ dappnode_core_download()
             # Download DAppNode Core docker-compose yml files if it's needed
             eval "[ -f \$${comp}_YML_FILE ] || $WGET -O \$${comp}_YML_FILE \$${comp}_YML"
             # Download DAppNode Core env files if it's needed
-            eval "[ -f \$${comp}_ENV_FILE ] || wget -O/dev/null -q \$${comp}_ENV && $WGET -O \$${comp}_ENV_FILE \$${comp}_ENV"
+            eval "[ -f \$${comp}_ENV_FILE ] || $WGET -O/dev/null -q \$${comp}_ENV && $WGET -O \$${comp}_ENV_FILE \$${comp}_ENV"
             # Download DAppNode Core env files if it's needed
             eval "[ -f \$${comp}_MANIFEST_FILE ] || $WGET -O \$${comp}_MANIFEST_FILE \$${comp}_MANIFEST"
         fi
