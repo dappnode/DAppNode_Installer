5d1449b419d559d8956512c01a8aa6229f47d6b3
vdo <vdo@greyfaze.net>
2020-04-10 17:36:45 +0200

    Check content hash

--- build/dappnode/scripts/dappnode_install.sh
+++ build/dappnode/scripts/dappnode_install.sh
@@ -5,7 +5,8 @@ DAPPNODE_CORE_DIR="${DAPPNODE_DIR}/DNCORE"
 LOGFILE="${DAPPNODE_DIR}/dappnode_install.log"
 MOTD_FILE="/etc/motd"
 PKGS=(BIND IPFS VPN WAMP DAPPMANAGER ADMIN WIFI)
-CONTENT_HASH_PKGS=(geth openethereum)
+CONTENT_HASH_PKGS=(geth)
+#CONTENT_HASH_PKGS=(geth openethereum nethermind)
 CONTENT_HASH_FILE="${DAPPNODE_CORE_DIR}/packages-content-hash.csv"
 
 if [ "$UPDATE" = true ]; then
@@ -198,8 +199,8 @@ installExtra() {
 grabContentHashes() {
     >${DAPPNODE_CORE_DIR}/packages-content-hash.csv
     for comp in "${CONTENT_HASH_PKGS[@]}"; do
-        CONTENT_HASH=$(eval ${SWGET} https://github.com/dappnode/DAppNodePackage-${comp}/releases/latest/download/content-hash) 2>&1 | tee -a $LOGFILE
-        if [ -z "${CONTENT_HASH}"]; then
+        CONTENT_HASH=$(eval ${SWGET} https://github.com/dappnode/DAppNodePackage-${comp}/releases/latest/download/content-hash)
+        if [ -z $CONTENT_HASH ]; then
             echo "ERROR! Failed to find content hash of ${comp}." 2>&1 | tee -a $LOGFILE
             exit 1
         fi
