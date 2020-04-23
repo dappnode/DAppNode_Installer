b7e3704b7fe006e30962bf45d433eb09dde469c1
vdo <vdo@greyfaze.net>
2020-04-23 12:45:21 +0200

    Point to master

--- build/dappnode/scripts/dappnode_install.sh
+++ build/dappnode/scripts/dappnode_install.sh
@@ -23,7 +23,7 @@ mkdir -p $DAPPNODE_CORE_DIR
 mkdir -p "${DAPPNODE_CORE_DIR}/scripts"
 mkdir -p "${DAPPNODE_DIR}/config"
 
-PROFILE_BRANCH="v0.2.27"
+PROFILE_BRANCH="master"
 PROFILE_URL="https://raw.githubusercontent.com/dappnode/DAppNode_Installer/${PROFILE_BRANCH}/build/scripts/.dappnode_profile"
 PROFILE_FILE="${DAPPNODE_CORE_DIR}/.dappnode_profile"
 WGET="wget -q --show-progress --progress=bar:force"
