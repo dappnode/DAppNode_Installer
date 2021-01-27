00df81845fce5ee3be56b0173f882aeb0db02a3a
Eduardo Antuña Díez <eduadiez@gmail.com>
2021-01-27 15:38:31 +0100

    Add IPFS download feature

--- build/dappnode/scripts/dappnode_install.sh
+++ build/dappnode/scripts/dappnode_install.sh
@@ -31,6 +31,7 @@ PROFILE_URL="https://raw.githubusercontent.com/dappnode/DAppNode_Installer/${PRO
 DAPPNODE_PROFILE="${DAPPNODE_CORE_DIR}/.dappnode_profile"
 WGET="wget -q --show-progress --progress=bar:force"
 SWGET="wget -q -O-"
+IPFS_ENDPOINT=${IPFS_ENDPOINT:-"http://ipfs.io"}
 
 function valid_ip() {
     local ip=$1
@@ -63,13 +64,19 @@ source "${DAPPNODE_PROFILE}"
 
 # The indirect variable expansion used in ${!ver##*:} allows us to use versions like 'dev:development'
 # If such variable with 'dev:'' suffix is used, then the component is built from specified branch or commit.
+# you can also specify an IPFS version like /ipfs/QmWg8P2b9JKQ8thAVz49J8SbJbCoi2MwkHnUqMtpzDTtxR:0.2.7, it's important
+# to include the exact version also in the IPFS hash format since it's needed to be able to download it 
 for comp in "${PKGS[@]}"; do
     ver="${comp}_VERSION"
-    eval "${comp}_URL=\"https://github.com/dappnode/DNP_${comp}/releases/download/v${!ver}/${comp,,}.dnp.dappnode.eth_${!ver}_linux-${ARCH}.txz\""
-    eval "${comp}_YML=\"https://github.com/dappnode/DNP_${comp}/releases/download/v${!ver}/docker-compose.yml\""
-    eval "${comp}_MANIFEST=\"https://github.com/dappnode/DNP_${comp}/releases/download/v${!ver}/dappnode_package.json\""
+    DOWNLOAD_URL="https://github.com/dappnode/DNP_${comp}/releases/download/v${!ver}"
+    if [[ ${!ver} == /ipfs/* ]]; then
+        DOWNLOAD_URL=${IPFS_ENDPOINT}/api/v0/cat?arg=${!ver%:*}
+    fi
+    eval "${comp}_URL=\"${DOWNLOAD_URL}/${comp,,}.dnp.dappnode.eth_${!ver##*:}_linux-${ARCH}.txz\""
+    eval "${comp}_YML=\"${DOWNLOAD_URL}/docker-compose.yml\""
+    eval "${comp}_MANIFEST=\"${DOWNLOAD_URL}/dappnode_package.json\""
     eval "${comp}_YML_FILE=\"${DAPPNODE_CORE_DIR}/docker-compose-${comp,,}.yml\""
-    eval "${comp}_FILE=\"${DAPPNODE_CORE_DIR}/${comp,,}.dnp.dappnode.eth_${!ver}_linux-${ARCH}.txz\""
+    eval "${comp}_FILE=\"${DAPPNODE_CORE_DIR}/${comp,,}.dnp.dappnode.eth_${!ver##*:}_linux-${ARCH}.txz\""
     eval "${comp}_MANIFEST_FILE=\"${DAPPNODE_CORE_DIR}/dappnode_package-${comp,,}.json\""
 done
 
@@ -102,11 +109,14 @@ dappnode_core_download() {
         ver="${comp}_VERSION"
         if [[ ${!ver} != dev:* ]]; then
             # Download DAppNode Core Images if it's needed
-            eval "[ -f \$${comp}_FILE ] || $WGET -O \$${comp}_FILE \$${comp}_URL"
+            echo "Downloading ${comp} tar..."
+            eval "[ -f \$${comp}_FILE ] || $WGET -O \$${comp}_FILE \$${comp}_URL || exit 1"
             # Download DAppNode Core docker-compose yml files if it's needed
-            eval "[ -f \$${comp}_YML_FILE ] || $WGET -O \$${comp}_YML_FILE \$${comp}_YML"
-            # Download DAppNode Core manifest files if it's needed
-            eval "[ -f \$${comp}_MANIFEST_FILE ] || $WGET -O \$${comp}_MANIFEST_FILE \$${comp}_MANIFEST"
+            echo "Downloading ${comp} yml..."
+            eval "[ -f \$${comp}_YML_FILE ] || $WGET -O \$${comp}_YML_FILE \$${comp}_YML || exit 1";
+	    # Download DAppNode Core manifest files if it's needed
+            echo "Downloading ${comp} manifest..."
+            eval "[ -f \$${comp}_MANIFEST_FILE ] || $WGET -O \$${comp}_MANIFEST_FILE \$${comp}_MANIFEST || exit 1";
         fi
     done
 }
