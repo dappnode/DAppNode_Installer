314298e4d037700fae221171b18a0c7043b774c8
pablo <mendez4a@gmail.com>
2021-04-07 15:59:29 +0200

    removed ansi color

--- build/dappnode/scripts/dappnode_install.sh
+++ build/dappnode/scripts/dappnode_install.sh
@@ -165,7 +165,7 @@ dappnode_start() {
     # To execute `compose-up` against more than 1 compose, composes files must share compose file version (e.g 3.5)
     for comp in "${DNCORE_YMLS[@]}"; do
         docker-compose $comp up -d 2>&1 | tee -a $LOGFILE
-        echo -e "\e[32m${comp} started\e[0m" 2>&1 | tee -a $LOGFILE
+        echo "${comp} started" 2>&1 | tee -a $LOGFILE
     done
     echo -e "\e[32mDAppNode started\e[0m" 2>&1 | tee -a $LOGFILE
 
