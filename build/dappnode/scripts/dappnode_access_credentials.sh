cbda02374779f13f10ae79f37ca0de40a2313022
pablo <mendez4a@gmail.com>
2021-05-10 18:19:25 +0200

    Added script to installer

--- build/dappnode/scripts/dappnode_access_credentials.sh
+++ build/dappnode/scripts/dappnode_access_credentials.sh
@@ -7,15 +7,16 @@
 #############
 # Containers
 WIFI_CONTAINER="DAppNodeCore-wifi.dnp.dappnode.eth"
-WIREGUARD_CONTAINER="DAppNodeCore-wireguard.wireguard.dnp.dappnode.eth"
+WIREGUARD_CONTAINER="DAppNodeCore-wireguard.dnp.dappnode.eth"
 OPENVPN_CONTAINER="DAppNodeCore-vpn.dnp.dappnode.eth"
+HTTPS_CONTAINER="DAppNodeCore-https.dnp.dappnode.eth"
 # Credentials
 WIREGUARD_CREDS="docker exec $WIREGUARD_CONTAINER cat /config/peer_dappnode_admin/peer_dappnode_admin.conf"
 OPENVPN_CREDS="docker exec -i $OPENVPN_CONTAINER getAdminCredentials"
 WIFI_CREDS=$(cat /usr/src/dappnode/DNCORE/docker-compose-wifi.yml 2> /dev/null | grep 'SSID\|WPA_PASSPHRASE')
 # Endpoints
 DAPPNODE_ENDPOINT="http://my.dappnode"
-DAPPNODE_AVAHI_ENDPOINT="my.dappnode.local"
+DAPPNODE_AVAHI_ENDPOINT="http://my.dappnode.local"
 
 #############
 #1.FUNCTIONS#
@@ -36,7 +37,7 @@ function wifi_connection () {
 function avahi_connection () {
   # Ping to avahi endpoint && avahi-daemon running => systemctl is-active avahi-daemon RETURNS "active" or "inactive"
   ping -c 1 -w 10000 $DAPPNODE_AVAHI_ENDPOINT > /dev/null 2>&1 || { echo "Avahi-daemon not available" ; return ; }
-  [ "$(systemctl is-active avahi-daemon)" = "active" ] &&  echo -e "Connect to DAppNode through avahi-daemon.\n$2\nVisit \e[4m$DAPPNODE_AVAHI_ENDPOINT\e" && exit 0 || echo "Avahi-daemon not available"
+  [ "$(docker ps -a | grep ${HTTPS_CONTAINER})" ] && [ "$(docker inspect -f '{{.State.Running}}' ${HTTPS_CONTAINER})" = "true" ] && [ "$(systemctl is-active avahi-daemon)" = "active" ] &&  echo -e "Connect to DAppNode through avahi-daemon.\n$2\nVisit \e[4m$DAPPNODE_AVAHI_ENDPOINT\e" && exit 0 || echo "Avahi-daemon not available"
 }
 
 function wireguard_connection () {
