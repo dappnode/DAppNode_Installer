976ffb06010586e2135814715e19291837de016b
pablo <mendez4a@gmail.com>
2021-05-13 13:17:11 +0200

    install ping tool

--- test/environment_setup.sh
+++ test/environment_setup.sh
@@ -5,8 +5,14 @@
 ##########################
 
 # Docker should be uninstalled
-apt-get purge docker-ce docker-ce-cli containerd.io
+# apt-get purge docker-ce docker-ce-cli containerd.io ==> NOT able to uninstall docker on github host
 
 # Create necessary folder
 mkdir -p /etc/network/
 echo "iface en.x inet dhcp" >> /etc/network/interfaces
+
+
+##########################
+# dappnode_test_install.sh#
+##########################
+apt install iputils-ping
