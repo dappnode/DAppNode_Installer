621a849bbba784fec9b9e204d31190587863ff47
pablo <mendez4a@gmail.com>
2021-06-03 13:12:52 +0200

    improved acces_credentials.sh script

--- scripts/dappnode_access_credentials.sh
+++ scripts/dappnode_access_credentials.sh
@@ -9,7 +9,7 @@
 #############
 # Containers
 WIFI_CONTAINER="DAppNodeCore-wifi.dnp.dappnode.eth"
-WIREGUARD_CONTAINER="DAppNodeCore-wireguard.dnp.dappnode.eth"
+WIREGUARD_CONTAINER="DAppNodeCore-wireguard.wireguard.dnp.dappnode.eth"
 OPENVPN_CONTAINER="DAppNodeCore-vpn.dnp.dappnode.eth"
 HTTPS_CONTAINER="DAppNodeCore-https.dnp.dappnode.eth"
 # Credentials
@@ -17,8 +17,9 @@ WIREGUARD_GET_CREDS="docker exec $WIREGUARD_CONTAINER cat /config/peer_dappnode_
 OPENVPN_GET_CREDS="docker exec -i $OPENVPN_CONTAINER getAdminCredentials"
 WIFI_GET_CREDS=$(cat /usr/src/dappnode/DNCORE/docker-compose-wifi.yml 2> /dev/null | grep 'SSID\|WPA_PASSPHRASE')
 # Endpoints
+AVAHI_ENDPOINT="dappnode.local"
 DAPPNODE_ADMINUI_URL="http://my.dappnode"
-DAPPNODE_ADMINUI_LOCAL_URL="http://dappnode.local"
+DAPPNODE_ADMINUI_LOCAL_URL="http://${AVAHI_ENDPOINT}"
 DAPPNODE_WELCOME_URL="http://welcome.dappnode"
 
 #############
@@ -36,25 +37,23 @@ function dappnode_startup_delay () {
 
 # $1 Connection method $2 Credentials
 function create_connection_message () {
-  echo -e "\e[32mConnect to DAppNode through $1 using the following credentials:\e[0m\n$2\nVisit \e[4m$DAPPNODE_ADMINUI_URL\e\nCheck out all the access methods available to connect to your DAppNode at \e[4m$DAPPNODE_WELCOME_URL\e"
+  echo -e "\e[32mConnect to DAppNode through $1 using the following credentials:\e[0m\n$2\nVisit \e[4m$DAPPNODE_ADMINUI_URL\e\n[0mCheck out all the access methods available to connect to your DAppNode at \e[4m$DAPPNODE_WELCOME_URL\e"
 }
 
 function wifi_connection () {
   # NOTE: network interface may be in use by host => $(cat /sys/class/net/${INTERFACE}/operstate)" !== "up")
   # NOTE: wifi has a delay up to 1 min
-  # INTERFACE variable
-  INTERFACE=$(ls /sys/class/ieee80211/*/device/net 2> /dev/null)
   # wifi container running
   [ "$(docker inspect -f '{{.State.Running}}' ${WIFI_CONTAINER} 2> /dev/null)" = "true" ] && \
   # Check interface variable is set
-  [ ! -z $INTERFACE ] && \
+  [ $(docker exec -it $WIFI_CONTAINER iw dev | grep 'Interface' | awk 'NR==1{print $2}') ] && \
   create_connection_message "Wi-Fi" "$WIFI_GET_CREDS" && \
   exit 0 || echo "Wifi not available"
 }
 
 function avahi_connection () {
   # Ping to avahi endpoint: -c: number of pings. -w: timeout
-  ping -c 2 -w 10 dappnode.local > /dev/null 2>&1 || { echo "Avahi-daemon not available" ; return ; }
+  avahi-resolve -n $AVAHI_ENDPOINT > /dev/null 2>&1 || { echo "Avahi-daemon not available" ; return ; }
   # Https container exists
   [ "$(docker ps -a | grep ${HTTPS_CONTAINER})" ] && \
   # Https container running
